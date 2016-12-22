a6de0b8ae8b0b193bc4bedff85cc5b3bdf844d0d
Phillip Freitas <phillipjonathan@gmail.com>
2016-12-22 15:00:31 -0200

    Create new pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,25 @@
 class PicsController < ApplicationController
+	#before_action :find_params, only: [:index, :new,] 
 
 	def index
 		
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_params
+		@pic = Pic.find(pic_params[:id])
+	end
 end
